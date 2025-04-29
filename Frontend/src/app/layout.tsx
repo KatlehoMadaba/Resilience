d5df4bd52ddc8dab07624acca4c1bc85032d61c1
KatlehoMadaba <101579604+KatlehoMadaba@users.noreply.github.com>
2025-04-29 18:53:23 +0200

    providers order stepup

--- Frontend/src/app/layout.tsx
+++ Frontend/src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { AuthProvider } from "@/providers/auth-provider";
+import { UserProvider } from "@/providers/users-providers";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +29,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <UserProvider>
+          <AuthProvider>{children}</AuthProvider>
+        </UserProvider>
       </body>
     </html>
   );
