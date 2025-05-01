36a87f37700d1507b2c4d097ce7498984fe1861c
KatlehoMadaba <101579604+KatlehoMadaba@users.noreply.github.com>
2025-05-01 08:42:21 +0200

    move medical facility business logic to manager

---
+++ aspnet-core/src/Resilience.Core/Domain/Medical AssistanceRecords/MedicalFacilityManager.cs
@@ -0,0 +1,48 @@
+﻿using Abp.Application.Services.Dto;
+using Abp.Domain.Repositories;
+using Abp.Domain.Services;
+using Abp.ObjectMapping;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Resilience.Domain.Medical_AssistanceRecords
+{
+    public class MedicalFacilityManager:DomainService
+    {
+        private readonly IRepository<MedicalFacility, Guid> _repository;
+
+        public MedicalFacilityManager(IRepository<MedicalFacility, Guid> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<List<MedicalFacility>>GetPagedDistinctMedicalFacilitiesAsync(int skipCount, int maxResultCount)
+        {
+            var queryable = await _repository.GetAllAsync();
+
+            var distinctQuery = queryable
+                .ToList()
+                .DistinctBy(x => x.PlaceId)
+                .OrderBy(x => x.Name)
+                .Skip(skipCount)
+                .Take(maxResultCount)
+                .ToList();
+            return distinctQuery;
+        }
+        public async Task<int> GetDistinctCountAsync()
+        {
+            var queryable = await _repository.GetAllAsync();
+
+            var count = queryable
+                .ToList()
+                .DistinctBy(x => x.PlaceId)
+                .Count();
+
+            return count;
+        }
+    }
+}
+
