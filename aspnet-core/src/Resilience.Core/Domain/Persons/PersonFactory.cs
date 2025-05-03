16e46402f28f544f795ec4af71fa556cd263ea72
KatlehoMadaba <101579604+KatlehoMadaba@users.noreply.github.com>
2025-05-03 20:08:53 +0200

    PersonFactory in Process

---
+++ aspnet-core/src/Resilience.Core/Domain/Persons/PersonFactory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Abp.Domain.Repositories;
+using Resilience.Authorization.Users;
+
+namespace Resilience.Domain.Persons
+{
+    public class PersonFactory
+    {
+        private readonly UserManager _userManager;
+        private readonly IRepository<GeneralSupporter, Guid> _generalSupporterRepository;
+        private readonly IRepository<ImmediateSurvivor, Guid> _imdsurvivorRepository;
+        private readonly IRepository<PastSurvivor, Guid> _pastRepository;
+
+    }
+}
