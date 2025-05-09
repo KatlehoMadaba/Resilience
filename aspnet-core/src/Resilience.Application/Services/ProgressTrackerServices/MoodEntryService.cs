90693240f6602cbc7ff06cb432d4837871c5d648
KatlehoMadaba <101579604+KatlehoMadaba@users.noreply.github.com>
2025-05-09 15:16:50 +0200

    Created crud for mood entry

--- aspnet-core/src/Resilience.Application/Services/ProgressTrackerServices/MoodEntryService.cs
+++ aspnet-core/src/Resilience.Application/Services/ProgressTrackerServices/MoodEntryService.cs
@@ -1,6 +1,28 @@
-﻿namespace Resilience.Services.ProgressTrackerServices
+﻿using System;
+using System.Threading.Tasks;
+using Abp.Application.Services;
+using Abp.Application.Services.Dto;
+using Abp.Domain.Repositories;
+using AutoMapper;
+using Resilience.Domain.ProgressTrackers;
+using Resilience.Services.ProgressTrackerServices.Dtos;
+
+namespace Resilience.Services.ProgressTrackerServices
 {
-    public class MoodEntryService
+    public class MoodEntryService : AsyncCrudAppService<MoodEntry, MoodEntryDto, Guid, PagedAndSortedResultRequestDto, MoodEntryDto>
     {
+        
+        private readonly MoodEntryManager _moodEntryManager;
+        private readonly IRepository<MoodEntry, Guid> _moodentryrespository;
+        private readonly IMapper _mapper;
+
+        public MoodEntryService(IRepository<MoodEntry, Guid> repository, MoodEntryManager moodEntryManager, IMapper mapper, IRepository<MoodEntry, Guid> moodentryrespository) : base(repository)
+        {
+            _moodEntryManager = moodEntryManager;
+            _mapper = mapper;
+            _moodentryrespository = moodentryrespository;
+        }
     }
 }
+
+
--- aspnet-core/src/Resilience.Core/Domain/ProgressTrackers/ProgressTrackerManager.cs
+++ aspnet-core/src/Resilience.Core/Domain/ProgressTrackers/ProgressTrackerManager.cs
@@ -1,7 +1,16 @@
-﻿namespace Resilience.Domain.ProgressTrackers
+﻿using Abp.Domain.Repositories;
+using System;
+using Abp.Domain.Services;
+using Resilience.Domain.PoliceStations;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Castle.MicroKernel.Registration;
+using Abp.UI;
+
+namespace Resilience.Domain.ProgressTrackers
 {
-    public class ProgressTrackerManager
+    public class ProgressTrackerManager 
     {
-     
+
     }
 }
