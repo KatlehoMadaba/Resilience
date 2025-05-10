c76cc1da03bb24e4ef6d204869a9007c39f2058d
KatlehoMadaba <101579604+KatlehoMadaba@users.noreply.github.com>
2025-05-10 22:54:07 +0200

    Testimony submistion created with a simple button

--- Frontend/src/app/survivor/testimony/page.tsx
+++ Frontend/src/app/survivor/testimony/page.tsx
@@ -1,9 +1,11 @@
+"use client"
+import TestimonyForm from "@/components/testimony/Testimony";
 import React from "react";
 
 const TestimonyPage = () => {
   return (
     <div>
-      <h1>Testmony</h1>
+      <TestimonyForm />
     </div>
   );
 };
