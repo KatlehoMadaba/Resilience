ee28084eef7a848de8ef90aa99f413ee197960eb
KatlehoMadaba <101579604+KatlehoMadaba@users.noreply.github.com>
2025-05-11 08:14:28 +0200

    chore:updated routing

--- Frontend/src/hoc/withAuth.tsx
+++ Frontend/src/hoc/withAuth.tsx
@@ -10,15 +10,15 @@ interface LayoutProps {
 const withAuth = (WrappedLayout: React.ComponentType<LayoutProps>) => {
   const WithAuthWrapper: React.FC<LayoutProps> = ({ children, ...props }) => {
     const router = useRouter();
-
     useEffect(() => {
-      const token = sessionStorage.getItem("jwt");
       if (!token) {
         router.push("/login");
         return;
       }
+
       try {
-          const role = getRole(token);
+        const token = sessionStorage.getItem("jwt");
+        const role = getRole(token);
         if (role === "generalsupporter") {
           router.push("/supporter");
         } else if (role === "professional") {
