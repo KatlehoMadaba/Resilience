e9ea233f829f48f9568f6feb23b5f4dcdc410a25
KatlehoMadaba <101579604+KatlehoMadaba@users.noreply.github.com>
2025-05-11 14:25:06 +0200

    feature:created app service

---
+++ aspnet-core/src/Resilience.Application/Services/PersonServices/Dtos/ProfessionalResponseDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Abp.Application.Services.Dto;
+using Resilience.Domain.Persons;
+
+namespace Resilience.Services.PersonServices.Dtos
+{
+    public class ProfessionalResponseDto : FullAuditedEntityDto<Guid>
+    {
+        public long UserId { get; set; }
+        public string? UserName { get; set; }
+        public string? Name { get; set; }
+        public string? Surname { get; set; }
+        public string? Password { get; set; }
+        public string? EmailAddress { get; set; }
+        public ReflistSex? Sex { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string Profession { get; set; }
+        public string Organization { get; set; }
+        public string Credentials { get; set; }
+        public bool IsVerified { get; set; }
+        public bool isActive { get; set; }
+    }
+}
---
+++ aspnet-core/src/Resilience.Application/Services/PersonServices/ProfessionalAppService.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Threading.Tasks;
+using Abp.Application.Services;
+using Abp.Application.Services.Dto;
+using Abp.Domain.Repositories;
+using Abp.UI;
+using AutoMapper;
+using Resilience.Domain.Persons;
+using Resilience.Services.PersonServices.Dtos;
+
+namespace Resilience.Services.PersonServices
+{
+    public class ProfessionalAppService :
+      AsyncCrudAppService<Professional, ProfessionalResponseDto, Guid, PagedAndSortedResultRequestDto, ProfessionalRequestDto, ProfessionalResponseDto>
+
+    {
+        private readonly ProfessionalManager _ProfessionalManager;
+        private readonly IRepository<Professional, Guid> _Imdrepository;
+        private readonly IMapper _mapper;
+        public ProfessionalAppService(IRepository<Professional, Guid> repository, ProfessionalManager ProfessionalManager, IMapper mapper, IRepository<Professional, Guid> imdrepository) : base(repository)
+        {
+            _ProfessionalManager = ProfessionalManager;
+            _mapper = mapper;
+            _Imdrepository = imdrepository;
+        }
+        public override async Task<ProfessionalResponseDto> CreateAsync(ProfessionalRequestDto input)
+        {
+            var professional = await _ProfessionalManager.CreateProfessionalAsync(
+                input.Name,
+                input.Surname,
+                input.EmailAddress,
+                input.UserName,
+                input.Password,
+                input.Sex,
+                input.PhoneNumber,
+                input.Profession,        
+                input.Organization,       
+                input.Credentials,        
+                input.IsVerified,         
+                input.isActive          
+            );
+
+            return _mapper.Map<ProfessionalResponseDto>(professional);
+        }
+
+        protected override async Task<Professional> GetEntityByIdAsync(Guid id)
+        {
+            var Professional = await _ProfessionalManager.GetProfessionalByIdWithUserAsync(id);
+            if (Professional == null)
+            {
+                throw new UserFriendlyException("Professional not found");
+            }
+            return Professional;
+        }
+
+        public async Task<ProfessionalResponseDto> GetCurrentSurvivorAsync(long userId)
+        {
+            var Professional = await _ProfessionalManager.GetProfessionalByUserIdAsync(userId);
+            return _mapper.Map<Professional, ProfessionalResponseDto>(Professional);
+        }
+
+
+    }
+}
