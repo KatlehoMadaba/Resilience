b52668d82f49efe5b2af9ebe5cb7310eec5b544f
KatlehoMadaba <101579604+KatlehoMadaba@users.noreply.github.com>
2025-05-14 00:46:28 +0200

    feature:detemining the user who is logged in

--- Frontend/src/app/survivor/humanTherapist/page.tsx
+++ Frontend/src/app/survivor/humanTherapist/page.tsx
@@ -1,23 +1,22 @@
 import ChatInterface from "@/components/chat/ChatInterface";
 import ProfessionalContacts from "@/components/chat/ProfessionalContacts";
-
-// interface Props {
-//   params: {
-//     personId: string;
-//   };
-// }
-
-// export default function ChatPage({ params }: Props) {
-//   return <ChatInterface personId={params.personId} />;
-// }
-
+import SurviorContacts from "@/components/chat/SurviorContacts";
 import React from "react";
+import { useSurvivorActions } from "@/providers/survivors-provider/index";
+import { useSurvivorState } from "@/providers/survivors-provider/index";
+import { useSurvState } from '@/providers/professionals-provider';
+import { useState } from "react";
 
 const HumanTherapist = () => {
+  const { Survivor } = useSurvivorState();
+  const { getCurrentSurvivor } = useSurvivorActions();
+  const [loggedInUser,setLoggedinUser]=useState("professional")
+
   return (
     <div>
       <ChatInterface />
       <ProfessionalContacts />
+      <SurviorContacts/>
     </div>
   );
 };
