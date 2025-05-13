972bc9a99ad2f5e1609a6b73faf5c9857271fc08
KatlehoMadaba <101579604+KatlehoMadaba@users.noreply.github.com>
2025-05-13 07:48:48 +0200

    fix:added use client

--- Frontend/src/app/survivor/humanTherapist/page.tsx
+++ Frontend/src/app/survivor/humanTherapist/page.tsx
@@ -1,3 +1,4 @@
+"use client"
 import { useState } from "react";
 import ChatInterface from "@/components/chat/ChatInterface";
 import ProfessionalContacts from "@/components/chat/ProfessionalContacts";
