4fecdba7f610c17de3018eb5c704c9761375a1aa
KatlehoMadaba <101579604+KatlehoMadaba@users.noreply.github.com>
2025-05-14 13:05:04 +0200

    feature:using exsiting asnyc cruds

--- aspnet-core/src/Resilience.Application/Services/ReportsServices/SexualAssaultReportAppService.cs
+++ aspnet-core/src/Resilience.Application/Services/ReportsServices/SexualAssaultReportAppService.cs
@@ -1,83 +1,24 @@
 ﻿using System;
-using System.Threading.Tasks;
 using Abp.Application.Services;
 using Abp.Application.Services.Dto;
 using Abp.Domain.Repositories;
-using AutoMapper;
-using Resilience.Domain.Persons;
 using Resilience.Domain.Reports;
 using Resilience.Services.ReportsServices.Dtos;
 
 namespace Resilience.Services.ReportsServices
 {
-    public class SexualAssaultReportAppService : AsyncCrudAppService<SexualAssaultReport, SexualAssaultReportDto, Guid, PagedAndSortedResultRequestDto, SexualAssaultReportDto>
+    public class SexualAssaultReportAppService : AsyncCrudAppService<SexualAssaultReport, SexualAssaultReportDto, Guid, PagedAndSortedResultRequestDto, SexualAssualtReportRequest, SexualAssaultReportDto>
     {
-        private SexaualAssualtReportManager _sexualAssualtReportmanager;
-        private readonly IMapper _mapper;
-        public SexualAssaultReportAppService(IRepository<SexualAssaultReport, Guid> repository, SexaualAssualtReportManager sexualAssualtReportmanager) : base(repository)
+ 
+       
+        public SexualAssaultReportAppService(IRepository<SexualAssaultReport, Guid> repository ) : base(repository)
         {
-            _sexualAssualtReportmanager = sexualAssualtReportmanager;
+          
         }
 
-        public override async Task<SexualAssaultReportDto> CreateAsync(SexualAssaultReportDto input)
-        {
-            var sexualAssualtReport = await _sexualAssualtReportmanager.CreateSexualReportAsync
-                (
-                    input.PersonId,
-                    input.ReportStatus,
-                    input.EncryptedContent,
-                    input.IsSharedWithAuthorities,
-                    input.SharedDate,
-                    input.FileReference,
 
-                    input.FullName,
-                    input.IDNumber,
-                    input.DateOfBirth,
-                    input.Address,
-                    input.PhoneNumber,
-                    input.Occupation,
 
-                    // Incident Details  
-                    input.IncidentDateTime,
-                    input.Location,
-                    input.AloneOrWithSomeone,
-                    input.LeadingEventsDescription,
-
-                    // Suspect Details  
-                    input.IsSuspectKnown,
-                    input.SuspectName,
-                    input.SuspectDescription,
-                    input.WeaponOrThreats,
-
-                    // Assault Details  
-                    input.AssaultDescription,
-                    input.Injuries,
-                    input.WordsSpokenBySuspect,
-
-                    // Post-Incident Actions  
-                    input.ActionsTaken,
-                    input.ChangedClothesOrShowered,
-                    input.ClothesKept,
-
-                    // Witnesses and Evidence  
-                    input.WitnessPresent,
-                    input.WitnessDetails,
-                    input.CCTVAvailable,
-                    input.IsOtherEvidence,
-                    input.OtherEvidenceDescription,
-
-                    // Medical  
-                    input.ReceivedMedicalAttention,
-                    input.WillingForensicExam,
-
-                    // Safety and Support  
-                    input.FeelsSafe,
-                    input.WantsCounsellor,
-                    input.PrefersFemaleOfficer
-                );
-            return _mapper.Map<SexualAssaultReportDto>(sexualAssualtReport);
-        }
-
+      
     }
 
 
