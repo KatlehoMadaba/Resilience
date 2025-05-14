695b5884b97256ae7f488b57b7161a6bd10fff38
KatlehoMadaba <101579604+KatlehoMadaba@users.noreply.github.com>
2025-05-15 00:34:46 +0200

    professional chat data retrival

--- Frontend/src/providers/chat-provider/context.tsx
+++ Frontend/src/providers/chat-provider/context.tsx
@@ -15,6 +15,7 @@ export interface IChatMessageStateContext {
 export interface IChatMessageActionContext {
   sendMessage: (sendMessage: ISendMessage) => void;
   getMessagesWithPerson: (personId: string) => void;
+  countMessageswithPersonId:()
 }
 
 // Initial state with default values
