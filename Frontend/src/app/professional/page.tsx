5cdc3142b98033c9a28c87d2e8c78b805c3b6d18
KatlehoMadaba <101579604+KatlehoMadaba@users.noreply.github.com>
2025-05-15 12:24:00 +0200

    bug:get current survivor debugging

--- Frontend/src/app/professional/page.tsx
+++ Frontend/src/app/professional/page.tsx
@@ -10,35 +10,54 @@ import {
   useChatMessageActions,
   useChatMessageState,
 } from "@/providers/chat-provider";
-import { useUserState } from "@/providers/users-providers";
+import { useRouter } from "next/navigation";
+import {
+  useProfessionalState,
+  useProfessionalActions,
+} from "@/providers/professionals-provider";
+import { useUserState, useUserActions } from "@/providers/users-providers";
 import Image from "next/image";
 import { useStyles } from "./styles";
-
 const { Title, Paragraph } = Typography;
 
 const ProfessionalDashboard = () => {
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
   const { styles } = useStyles();
-
   const { countMessages } = useChatMessageActions();
   const { CountMessages: messageCount } = useChatMessageState();
-  const { personId } = useUserState();
+  const { getCurrentUser } = useUserActions();
+  const { getCurrentProfessional } = useProfessionalActions();
+  const { currentProfessional } = useProfessionalState();
+  const router = useRouter();
 
   useEffect(() => {
-    if (personId) {
-      Promise.all([
-        countMessages(personId),
-        // countUniqueSurvivors(personId),
-      ]).finally(() => setLoading(false));
+    fetchSurvivorOnReload();
+  }, []);
+
+  const fetchSurvivorOnReload = async () => {
+    const token = sessionStorage.getItem("jwt");
+    if (!token) {
+      router.push("/login");
+      return;
     }
-  }, [personId]);
+    try {
+      setLoading(true);
+      const user = await getCurrentUser();
+      await getCurrentProfessional(user.id);
+      console.log("user", user.id, "this", currentProfessional?.name);
+    } catch (err) {
+      console.error("Error loading the Professional:", err);
+    } finally {
+      setLoading(false);
+    }
+  };
 
   return (
     <div className={styles.dashboardWrapper}>
       <Row gutter={[32, 32]} align="middle">
         <Col xs={24} md={12}>
           <div className={styles.headerText}>
-            <Title level={2}>Welcome, Doctor</Title>
+            <Title level={2}>Welcome, {currentProfessional?.name}</Title>
             <Paragraph className={styles.motivation}>
               Every message you send could be a turning point in someones
               healing. Your dedication is making real, lasting change.
