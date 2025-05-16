3069195a98c38ef82f730e3a25cbba5147d6ea6e
KatlehoMadaba <101579604+KatlehoMadaba@users.noreply.github.com>
2025-05-16 12:24:06 +0200

    route to police station

--- Frontend/src/app/Police/page.tsx
+++ Frontend/src/app/Police/page.tsx
@@ -1,14 +1,12 @@
-"use client";
 import NearByPoliceStations from "@/components/PoliceStation/page";
-
 import React from "react";
 
-const PoliceStations = () => {
+function page() {
   return (
     <>
       <NearByPoliceStations />
     </>
   );
-};
+}
 
-export default PoliceStations;
+export default page;
