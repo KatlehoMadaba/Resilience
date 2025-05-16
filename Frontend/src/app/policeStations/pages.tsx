20cab3d459ff90ee7f6756861f4f3392b5f6c2d5
KatlehoMadaba <101579604+KatlehoMadaba@users.noreply.github.com>
2025-05-16 02:36:50 +0200

    chore:clean up code

---
+++ Frontend/src/app/policeStations/pages.tsx
@@ -0,0 +1,14 @@
+"use client";
+import NearByPoliceStations from "@/components/PoliceStation/PoliceStations";
+
+import React from "react";
+
+const PoliceStations = () => {
+  return (
+    <>
+      <NearByPoliceStations />
+    </>
+  );
+};
+
+export default PoliceStations;
